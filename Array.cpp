227288d1cadd0e8dd0155391433ca2bb81210733
Roman Zuykin <ZyukinRY@mpei.ru>
2024-11-09 20:37:29 +0300

    dynamic arr

--- Array.cpp
+++ Array.cpp
@@ -1,51 +1,51 @@
-#include <iostream>
-#include <ctime>
-
-
-using namespace std;
-
- 
-void Swap(int *a,int *b) 
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-void FillArray(int *arr, const int size) 
-{
-	for (int i = 0; i < size; i++) {
-		arr[i] = rand() % 5000;
-	}
-}
-void PrintArray(int *arr, const int size)
-{
-	for (int i = 0; i < size; i++) {
-		cout << arr[i] << endl;
-	}
-}
-void BubbleSort(int *arr, const int size) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size-1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				/*int temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;*/
-				Swap(&arr[j], &arr[j+1]);
-			}
-		}
-	}
-}
-
-
-
-int main() {
-	srand(time(NULL));
-	const int size=20;
-	int arr[size];
-	FillArray(arr, size);
-	PrintArray(arr, size);
-	cout << "=========================" << endl;
-	BubbleSort(arr, size);
-	PrintArray(arr, size);
-	cout << "=========================" << endl;
-}
+//#include <iostream>
+//#include <ctime>
+//
+//
+//using namespace std;
+//
+// 
+//void Swap(int *a,int *b) 
+//{
+//	int temp = *a;
+//	*a = *b;
+//	*b = temp;
+//}
+//void FillArray(int *arr, const int size) 
+//{
+//	for (int i = 0; i < size; i++) {
+//		arr[i] = rand() % 5000;
+//	}
+//}
+//void PrintArray(int *arr, const int size)
+//{
+//	for (int i = 0; i < size; i++) {
+//		cout << arr[i] << endl;
+//	}
+//}
+//void BubbleSort(int *arr, const int size) {
+//	for (int i = 0; i < size; i++) {
+//		for (int j = 0; j < size-1; j++) {
+//			if (arr[j] > arr[j + 1]) {
+//				/*int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;*/
+//				Swap(&arr[j], &arr[j+1]);
+//			}
+//		}
+//	}
+//}
+//
+//
+//
+//int main() {
+//	srand(time(NULL));
+//	const int size=20;
+//	int arr[size];
+//	FillArray(arr, size);
+//	PrintArray(arr, size);
+//	cout << "=========================" << endl;
+//	BubbleSort(arr, size);
+//	PrintArray(arr, size);
+//	cout << "=========================" << endl;
+//}
---
+++ DynamicArray.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <ctime>
+//
+//
+//
+//template<typename T>void Swap(T &a, T &b) {
+//	T temp = a;
+//	a = b;
+//	b = temp;
+//}
+//void FillArray(int *arr, const int size) 
+//{
+//	for (int i = 0; i < size; i++) {
+//		arr[i] = rand() % 5000;
+//	}
+//}
+//void PrintArray(int *arr, const int size)
+//{
+//	for (int i = 0; i < size; i++) {
+//		std::cout << arr[i] << std::endl;
+//	}
+//}
+//int main() 
+//{
+//	srand(time(NULL));
+//	int size;
+//	std::cout << "Input Size = ";
+//	std::cin >> size;
+//	int *arr = new int[size];
+//	FillArray(arr, size);
+//	std::cout << "======================================" << std::endl;
+//	PrintArray(arr, size);
+//	
+//
+//	delete[] arr;
+//
+//}
